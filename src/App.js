3cc6dda20871cf491f8bafc58ab2ac0fc2ad0cc0
cobaltune <cobaltune@naver.com>
2023-03-30 20:21:30 +0900

    feat: router settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Todo from './pages/Todo';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/todo' element={<Todo />} />
+    </Routes>
   );
-}
+};
 
 export default App;
